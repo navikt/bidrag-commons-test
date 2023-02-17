87715595e9f53074d3dcc0fddb485668e7075ff8
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-02-17 15:00:26 +0100

    Konvertert til Kotlin

--- README.md
+++ README.md
@@ -8,19 +8,3 @@ Komponenter for testing som brukes på tvers av applikasjoner under bidrag
 
 Gjøres med 'workflows' og 'actions' fra GitHub. Se `.github/workflows/*` for detaljer. 
 
-## release endringer
-
-versjon | endringstype | beskrivelse
---------|--------------|-------------
-0.2.2   | endring      | Fjernet sårbarheter rapportert av snyk.io og oppgradert spring-boot  
-0.2.1   | endring      | Fjernet sårbarheter rapportert av snyk.io 
-0.2.0   | -- ingen --  | Overgang til maven repo fra github 
-0.1.0   | slettet      | `SecuredTestRestTemplate`: Fjernet avhengighet til spring-oidc-support fra nav 
-0.0.12  | endring      | `HttpHeaderTestRestTemplate.postForEntity(...)`
-0.0.12  | endring      | `HttpHeaderTestRestTemplate`: slettet konstruktør (kun brukt av `SecuredTestRestTemplate`) 
-0.0.11  | endring      | ny java baseline -> java 12
-0.0.10  | endring      | Adding custom headers only valid for one HttpEntityCallback 
-0.0.9   | endring      | `HttpHeaderTestRestTemplate` med alternativ metode for å legge på en custom header
-0.0.6   | opprettet    | `HttpHeaderTestRestTemplate` som kan konfigureres og som arves av `SecuredTestRestTemplate`
-0.0.4   | endring      | `SecuredTestRestTemplate` med public constructor
-0.0.1   | opprettet    | `SecuredTestRestTemplate`
---
+++ src/main/kotlin/no/nav/bidrag/commons/web/test/HttpHeaderTestRestTemplate.kt
@@ -0,0 +1,75 @@
+package no.nav.bidrag.commons.web.test
+
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import java.net.URI
+import java.util.*
+
+
+class HttpHeaderTestRestTemplate(val testRestTemplate: TestRestTemplate) {
+
+    private val headersForSingleCallbacks = Stack<Pair<String, String>>()
+    private val valueGenerators: MutableMap<String, ValueGenerator> = HashMap()
+    fun <T> exchange(url: String?, httpMethod: HttpMethod?, httpEntity: HttpEntity<*>, responseClass: Class<T>?): ResponseEntity<T> {
+        return testRestTemplate.exchange(url, httpMethod, newEntityWithAddedHeaders(httpEntity), responseClass)
+    }
+
+    fun <T> exchange(
+        url: String?,
+        httpMethod: HttpMethod?,
+        httpEntity: HttpEntity<*>?,
+        typeReference: ParameterizedTypeReference<T>?
+    ): ResponseEntity<T> {
+        return testRestTemplate.exchange(url, httpMethod, newEntityWithAddedHeaders(httpEntity), typeReference)
+    }
+
+    fun <T> postForEntity(url: String?, httpEntity: HttpEntity<*>, responseClass: Class<T>?): ResponseEntity<T> {
+        return testRestTemplate.postForEntity(url, newEntityWithAddedHeaders(httpEntity), responseClass)
+    }
+
+    inline fun <reified T : Any> getForEntity(uri: URI, request: Any?): HttpEntity<T> {
+        return testRestTemplate.exchange(uri, HttpMethod.GET, newEntityWithAddedHeaders(request))
+    }
+
+    inline fun <reified T : Any> putForEntity(uri: URI, request: Any?): HttpEntity<T> {
+        return testRestTemplate.exchange(uri, HttpMethod.PUT, newEntityWithAddedHeaders(request))
+    }
+
+    inline fun <reified T : Any> postForEntity(uri: URI, request: Any?): HttpEntity<T> {
+        return testRestTemplate.exchange(uri, HttpMethod.POST, newEntityWithAddedHeaders(request))
+    }
+
+    fun newEntityWithAddedHeaders(request: Any?): HttpEntity<*> {
+        val tempHeaders = HttpHeaders().apply {
+            valueGenerators.forEach { add(it.key, it.value.generate()) }
+        }
+
+        while (!headersForSingleCallbacks.empty()) {
+            val headerWithValue = headersForSingleCallbacks.pop()
+            tempHeaders.add(headerWithValue.first, headerWithValue.second)
+        }
+
+        if (request is HttpEntity<*>) {
+            return HttpEntity(request.body, tempHeaders.apply { putAll(request.headers) })
+        }
+        return HttpEntity(request, tempHeaders)
+    }
+
+    fun add(headerName: String, valueGenerator: ValueGenerator) {
+        valueGenerators[headerName] = valueGenerator
+    }
+
+    fun addHeaderForSingleHttpEntityCallback(headerName: String, headerValue: String) {
+        val headerWithValue = headerName to headerValue
+        headersForSingleCallbacks.push(headerWithValue)
+    }
+
+    fun interface ValueGenerator {
+        fun generate(): String?
+    }
+}
