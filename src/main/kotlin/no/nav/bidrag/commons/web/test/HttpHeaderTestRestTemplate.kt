79f5b970a71e674081303bd5ddbea1568f17026e
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-05-03 14:56:04 +0200

    Defaultverdier for nullable.

--- src/main/kotlin/no/nav/bidrag/commons/web/test/HttpHeaderTestRestTemplate.kt
+++ src/main/kotlin/no/nav/bidrag/commons/web/test/HttpHeaderTestRestTemplate.kt
@@ -32,15 +32,15 @@ class HttpHeaderTestRestTemplate(val testRestTemplate: TestRestTemplate) {
         return testRestTemplate.postForEntity(url, newEntityWithAddedHeaders(httpEntity), responseClass)
     }
 
-    inline fun <reified T : Any> getForEntity(uri: URI, request: Any?): ResponseEntity<T> {
+    inline fun <reified T : Any> getForEntity(uri: URI, request: Any? = null): ResponseEntity<T> {
         return testRestTemplate.exchange(uri, HttpMethod.GET, newEntityWithAddedHeaders(request))
     }
 
-    inline fun <reified T : Any> putForEntity(uri: URI, request: Any?): ResponseEntity<T> {
+    inline fun <reified T : Any> putForEntity(uri: URI, request: Any? = null): ResponseEntity<T> {
         return testRestTemplate.exchange(uri, HttpMethod.PUT, newEntityWithAddedHeaders(request))
     }
 
-    inline fun <reified T : Any> postForEntity(uri: URI, request: Any?): ResponseEntity<T> {
+    inline fun <reified T : Any> postForEntity(uri: URI, request: Any? = null): ResponseEntity<T> {
         return testRestTemplate.exchange(uri, HttpMethod.POST, newEntityWithAddedHeaders(request))
     }
 
